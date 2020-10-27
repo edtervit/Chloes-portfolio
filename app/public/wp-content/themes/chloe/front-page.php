6600d1993824d1f03d2642be06007c74422e26d7
Ed Tervit <edtervit@hotmail.co.uk>
2020-10-27 18:27:26 +0000

    new base

---
+++ app/public/wp-content/themes/chloe/front-page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The front-page template file
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Chloe
+ */
+
+get_header();
+?>
+
+	
+
+<?php
+get_footer();
---
+++ app/public/wp-content/themes/chloe/home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The Post post type template file
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Chloe
+ */
+
+get_header();
+?>
+
+	
+
+<?php
+get_footer();
